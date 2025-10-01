756ce5cfa863b08e0b78a70eaa442f1a9f9e62a6
timotejtalian <timotej.talian15@gmail.com>
2025-10-01 09:10:28 +0200

    a

--- prvocislo.py
+++ prvocislo.py
@@ -3,8 +3,9 @@ number=int(input("Zadaj cislo: "))
 countNumber=0
 for delitel in range (2,number) :
     if number % delitel == 0 :
-        countNumber += 1
+        countNumber += 1 #zvacsi premenu o 1
 if countNumber == 0 :
     print("Nie je to prvocislo")
 else :
     print("Nie je to prvocislo")
+    
